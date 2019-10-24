672d7fe7abb94568c46b48905cab1420cecae57d
SasinduDilshara <47134066+SasinduDilshara@users.noreply.github.com>
2019-10-24 15:15:59 +0530

    Update App.js

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
-import TodoItem from "./todo_components/Todoitem.js"
+// import TodoItem from "./todo_components/Todoitem.js"
 import ContactCard from "./contact_components/contactcard"
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <ContactCard>
-          name = "Sasindu"
-          phnnum = "011"
-          email = "jskjkj"
+      <ContactCard>
+        name = "Sasindu"
+        phnnum = "011"
+        email = "jskjkj"
         </ContactCard>
-      </div>
+
     );
   }
 }
